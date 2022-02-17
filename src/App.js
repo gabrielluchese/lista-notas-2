c7f1edc680fbfde71b2c3ba1ecae532b62caed09
gabrielluchese <gabrielluchese@hotmail.com>
2022-02-17 00:30:18 -0300

    Commit

---
+++ src/App.js
@@ -0,0 +1,36 @@
+import React, { Component } from "react";
+import ListaDeNotas from "./components/listaDeNotas";
+import FormularioCadastro from "./components/formularioCadastro";
+import "./assets/App.css";
+import './assets/index.css';
+
+class App extends Component {
+
+  constructor(){
+    super();
+
+    this.state = {
+      notas:[]
+    }
+  }
+
+  criarNota(titulo, texto){
+    const novaNota = {titulo, texto};
+    const novoArrayNotas = [...this.state.notas, novaNota]
+    const novoEstado = {
+      notas:novoArrayNotas
+    }
+    this.setState(novoEstado)
+  }
+
+  render(){
+    return (
+      <section className="conteudo">
+        <FormularioCadastro criarNota={this.criarNota.bind(this)} />
+        <ListaDeNotas notas={this.state.notas}/>
+      </section>
+    );
+  }
+}
+
+export default App;
